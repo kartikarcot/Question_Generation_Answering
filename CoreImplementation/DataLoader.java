5e61d954a13a3b2daf760c0493e8b2acbf80819d
Mansi Goyal <goyalmansi1996@gmail.com>
2021-03-27 09:51:51 -0400

    DataLoader module

---
+++ CoreImplementation/DataLoader.java
@@ -0,0 +1,43 @@
+//Use Absolute path to initialize the constructor
+//getText() returns the content of a file as a string
+
+
+
+package CoreImplementation;
+
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class DataLoader {
+
+    String content;
+
+    public DataLoader(String documentPath) {
+        initialize(documentPath);
+    }
+
+    public void initialize(String documentPath) {
+        Path path = Paths.get(documentPath);
+        try {
+            this.content = Files.readString(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println("Exception caught in DataLoader::Initialize");
+            e.printStackTrace();
+        }
+    }
+
+    public String getText()
+    {
+        return this.content;
+    }
+}
