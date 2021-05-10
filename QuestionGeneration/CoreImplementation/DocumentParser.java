adf1103adbaf43f58b8f1b3936ea71e4ca6bea4a
hzd11111 <alvin121212@live.cn>
2021-05-10 00:54:59 -0400

    faster parser

--- QuestionGeneration/CoreImplementation/DocumentParser.java
+++ QuestionGeneration/CoreImplementation/DocumentParser.java
@@ -35,13 +35,9 @@ public class DocumentParser {
         // set the list of annotators to run
         //props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
 
-         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref");
-
-        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
-        //MG: Options: neural, statistical
-        props.setProperty("coref.algorithm", "neural");
-
+         props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse");
         props.setProperty("ner.combinationMode", "HIGH_RECALL");
+        props.setProperty("parse.maxlen", "35");
 
         // build pipeline
         StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
@@ -51,7 +47,7 @@ public class DocumentParser {
         // System.out.println("Annotating the document");
         pipeline.annotate(originalDocument);
 
-        Annotation doc_annotations = originalDocument.annotation();
+        //Annotation doc_annotations = originalDocument.annotation();
 
         parsedSentences = new ArrayList<>();
         for (CoreSentence sentence : originalDocument.sentences()) {
@@ -60,84 +56,6 @@ public class DocumentParser {
                 continue;
             parsedSentences.add(p);
         }
-        // System.out.println("Finished annotating");
-
-//        // ToDo: Coreference Resolution
-//        Map<Integer, CorefChain> corefs = doc_annotations.get(CorefCoreAnnotations.CorefChainAnnotation.class);
-//
-//        originalParsedSentences = new ArrayList<ParsedSentence>();
-//
-//        StringBuilder corefResolvedSentenceDocument = new StringBuilder();
-//
-//        //Reference: https://stackoverflow.com/questions/30182138/how-to-replace-a-word-by-its-most-representative-mention-using-stanford-corenlp
-//        for (CoreSentence coreSentence: originalDocument.sentences()) {
-//
-//            CoreMap sentence = coreSentence.coreMap();
-//
-//            //Corresponding resolved sentence
-//            List<String> corefResolvedSentence = new ArrayList<String>();
-//
-//            //Parse through tokens for each of the sentence
-//            for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-//
-//                Integer ClusterId= token.get(CorefCoreAnnotations.CorefClusterIdAnnotation.class);
-//
-//                if(corefs.get(ClusterId)!=null){
-//
-//                    CorefChain coref_chain = corefs.get(ClusterId);
-////                    // System.out.println("matched chain = " + coref_chain);
-//                    //Fetch sentence index
-//                    int sentence_idx = coref_chain.getRepresentativeMention().sentNum -1;
-//                    CoreMap corefSentence = doc_annotations.get(CoreAnnotations.SentencesAnnotation.class).get(sentence_idx);
-//
-//                    List<CoreLabel> corefSentenceTokens = corefSentence.get(CoreAnnotations.TokensAnnotation.class);
-//                    CorefChain.CorefMention reprMent = coref_chain.getRepresentativeMention();
-////                    // System.out.println("reprMent :"+reprMent);
-////                    // System.out.println("Start index "+reprMent.startIndex);
-////                    // System.out.println("End Index "+reprMent.endIndex);
-//                    if (token.index() <= reprMent.startIndex || token.index() >= reprMent.endIndex) {
-//
-//                        for (int i = reprMent.startIndex; i < reprMent.endIndex; i++) {
-//                            CoreLabel matchedLabel = corefSentenceTokens.get(i - 1);
-//                            corefResolvedSentence.add(matchedLabel.word().replace("'s", ""));
-////                            // System.out.println("matchedLabel : "+matchedLabel.word());
-////                            // System.out.println("Token word "+token.word());
-//                        }
-//                    }
-//
-//                    else {
-//                        //No resolution needed
-//                        corefResolvedSentence.add(token.word());
-//                    }
-//
-//
-//                }else{
-//
-//                    //No resolution needed
-//                    corefResolvedSentence.add(token.word());
-//
-//
-//                }
-//
-//
-//            }
-//            ParsedSentence parsedObj = new ParsedSentence(coreSentence, corefResolvedSentence);
-//            originalParsedSentences.add(parsedObj);
-//            //// System.out.println(parsedObj.corefResolvedSentenceText);
-//            corefResolvedSentenceDocument.append(" "+parsedObj.corefResolvedSentenceText);
-//        }
-//
-//     //Added to annotate the coref resolved document
-//        // System.out.println(corefResolvedSentenceDocument.toString());
-//        document  = new CoreDocument(corefResolvedSentenceDocument.toString());
-//        // System.out.println("Annotated the coref resolved document");
-//        pipeline.annotate(document);
-//
-//        parsedSentences = new ArrayList<>();
-//        for (CoreSentence sentence : document.sentences()) {
-//            parsedSentences.add(new ParsedSentence(sentence));
-//        }
-//        // System.out.println("Finished annotating");
 
 
 
